fb69f0ab87b37f37172bb0ed5450591973bd8add
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-01-24 22:30:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ app.post('/capture', async (req, res) => {
         await page.goto(url, { waitUntil: 'networkidle2' });
 
         // Espera extra para garantir que o conteúdo dinâmico esteja carregado
-        await page.waitForTimeout(1000);  // Ajuste o tempo de espera conforme necessário
+        await page.waitFor(1000);  // Ajuste o tempo de espera conforme necessário
 
         console.log('Waiting for header and summary...');
         await page.waitForSelector('#header', { timeout: 20000 });
